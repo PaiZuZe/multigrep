c4df8edbe1ad1dd82c7b8961c27e1b6f8e8ff209
PaiZuZe <vitinho_cga@hotmail.com>
2019-03-22 21:09:34 -0300

    Add the option to specify teh directory.

--- bench.sh
+++ bench.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+folder=$HOME
+
+if [ ! -z $1 ]
+then
+    echo "Changing defaul folder to $1"
+    folder=$1
+    
+fi
+
 if [ ! -f "./pgrep" ]
 then
     make
@@ -9,7 +18,7 @@ cores=(1 2 4 8 16 64 128)
 for i in ${cores[*]}
 do
     exec 3>&1 4>&2
-    tempo=$( { /usr/bin/time -f "%e" ./pgrep $i i ~/Documents/3ºano/ >> /dev/null; } 2>&1 ) # change some_command
+    tempo=$( { /usr/bin/time -f "%e" ./pgrep $i i $folder >> /dev/null; } 2>&1 ) # change some_command
     exec 3>&- 4>&-
     echo "Using $i cores $tempo"
 done
