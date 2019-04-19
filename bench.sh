cec09351b1e946d6c589552584f4b292e15cb3a7
PaiZuZe <vitinho_cga@hotmail.com>
2019-04-19 00:10:15 -0300

    Change bench.sh, now it can take a output file.

--- bench.sh
+++ bench.sh
@@ -9,18 +9,39 @@ then
     
 fi
 
+if [ ! -z $2 ] && [ -f $2 ]
+then
+    echo "Deleting previos $2"
+    rm $2
+fi
+
 if [ ! -f "./pgrep" ]
 then
     make
 fi
 
-cores=(1 2 4 8 16 64 128)
+echo "make has finished"
+
+cores=(3 4 6 10 18 66 130)
 for i in ${cores[*]}
 do
-    exec 3>&1 4>&2
-    tempo=$( { /usr/bin/time -f "%e" ./pgrep $i i $folder 1>/dev/null; } 2>&1 ) # change some_command
-    exec 3>&- 4>&-
-    echo "Using $i cores $tempo"
+    sample_size=0
+    while [ $sample_size -lt 60 ]
+    do
+        exec 3>&1 4>&2
+        tempo=$( { /usr/bin/time -f "%e" ./pgrep $i i $folder 1>/dev/null; } 2>&1 ) # change some_command
+        exec 3>&- 4>&-
+        sample_size=$((sample_size+1))
+        
+        if [ -z $2 ]
+        then
+            echo Using $i cores $tempo
+            sample_size=60
+        else
+            echo $i $tempo >> $2
+        fi
+
+    done
 done
 
 make clean
